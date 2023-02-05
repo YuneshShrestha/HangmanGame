3e85b0938cfa64cfd3c3de2149be8688b7465e52
Yunesh Shrestha <52854563+YuneshShrestha@users.noreply.github.com>
2023-02-05 21:08:22 +0545

    displaying keys pressed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,16 +8,18 @@ public class GameManager : MonoBehaviour
     public Text timerText;
     public Text wordToFind;
     private float time;
-    private string[] textList = { "Cat", "Boat" ,"Horse" ,"Buffalo", "Game Manager"};
+    private string[] textList = { "CAT", "BOAT" ,"HORSE" ,"BUFFALO", "YO YO SO"};
+    private string textToDisplay;
+    private string hiddenText;
     // Start is called before the first frame update
     void Start()
     {
-        string textToDisplay = textList[Random.Range(0, textList.Length)];
-        string hiddenText="";
+        textToDisplay = textList[Random.Range(0,textList.Length)];
+        hiddenText="";
         for (int i = 0; i < textToDisplay.Length; i++) {
             if (char.IsWhiteSpace(textToDisplay[i]))
             {
-                hiddenText += " ";
+                hiddenText += "  ";
             }
             else {
                 hiddenText += "_ ";
@@ -25,6 +27,7 @@ public class GameManager : MonoBehaviour
 
         }
         wordToFind.text = hiddenText;
+        Debug.Log(textToDisplay.Length);
     }
 
     // Update is called once per frame
@@ -32,5 +35,23 @@ public class GameManager : MonoBehaviour
     {
         time += Time.deltaTime;
         timerText.text = "Timer:" + time.ToString();
+    }
+
+    private void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.type == EventType.KeyDown && e.keyCode.ToString().Length ==1) {
+            string pressedLetter = e.keyCode.ToString();
+            if (textToDisplay.Contains(pressedLetter)) {
+                int i = textToDisplay.IndexOf(pressedLetter);
+                Debug.Log(i);
+                while (i!=-1) {
+                    hiddenText = hiddenText.Substring(0, 2 * i) + pressedLetter + " " + hiddenText.Substring(2*(i+1)); 
+                    textToDisplay = textToDisplay.Substring(0,i) + "_"+  textToDisplay.Substring(i+1);
+                    i = textToDisplay.IndexOf(pressedLetter);
+                }
+                wordToFind.text = hiddenText;
+            }
+        }
     }
 }
